96016668cbef63bf34ed53c066198f5996bbe692
aline_zanin <aline_zanin@terceiros.sicredi.com.br>
2020-11-21 14:40:40 -0300

    inclusão projeto

---
+++ src/main/java/com/example/demo/entity/Pessoa.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Pessoa
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+
+}
